eba9cdb0766498e2c3897d805b8fab90a1745f81
Muya Guoji <guojimuya@gmail.com>
2023-05-04 17:48:02 -0400

    revised final test_view

--- captain_forever/test_view.py
+++ captain_forever/test_view.py
@@ -1,23 +1,27 @@
+"""Module for testing the print_text function in the view module."""
+
 import pytest
-import view
 import pygame
+import view
 
 
-@pytest.fixture
-def pygame_init():
+@pytest.fixture(name="pygame_fixture")
+def pygame_init_fixture():
+    """Initialize and cleanup the Pygame library."""
     pygame.init()
     yield
     pygame.quit()
 
 
-def test_print_text(pygame_init):
+def test_print_text(pygame_fixture):
     """
     Test the print_text function with real Pygame Surface and Font objects
     and checks if the message has been rendered on the surface by looking for
     the presence of the expected color in the surface's pixels.
 
     Args:
-        pygame_init: A pytest fixture that initializes and cleans up the Pygame library.
+        pygame_fixture: A pytest fixture that initializes and cleans up the
+        Pygame library.
     """
     # Create a real Pygame Surface object
     surface = pygame.Surface((800, 600))
@@ -35,10 +39,16 @@ def test_print_text(pygame_init):
     expected_color = pygame.Color("tomato")
     found_expected_color = False
 
-    for x in range(surface.get_width()):
-        for y in range(surface.get_height()):
-            if surface.get_at((x, y))[:3] == expected_color[:3]:
+    for x_position in range(surface.get_width()):
+        for y_position in range(surface.get_height()):
+            if (
+                surface.get_at((x_position, y_position))[:3]
+                == expected_color[:3]
+            ):
                 found_expected_color = True
                 break
+
+        if found_expected_color:
+            break
 
     assert found_expected_color, "Text not found in the rendered surface"
