5389849c3d3fb037b265726c68e075a328fd358f
jes-bro <91238623+jes-bro@users.noreply.github.com>
2023-05-05 00:56:54 -0400

    Delete residual var

--- captain_forever/controller.py
+++ captain_forever/controller.py
@@ -1,9 +1,11 @@
 # pylint: disable=no-member
+# pylint: disable=no-name-in-module
+# Disabling pylint warnings related to PyGame that aren't valid
 """
 Captain Forever controller.
 """
-import pygame
 from abc import ABC, abstractmethod
+import pygame
 
 
 class CaptainForeverController(ABC):
@@ -50,7 +52,7 @@ class ArrowController(CaptainForeverController):
     user input.
     """
 
-    def maneuver_player_ship(self, testing=False):
+    def maneuver_player_ship(self):
         """
         Move the player ship based on user input.
 
@@ -62,7 +64,6 @@ class ArrowController(CaptainForeverController):
             if event.type == pygame.QUIT or (
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
             ):
-                game_state.is_quitting = True
                 quit()
             elif (
                 game_state.player_ship
@@ -79,14 +80,13 @@ class ArrowController(CaptainForeverController):
             ):
                 game_state.__init__(self._width, self._height)
 
-        if not testing and game_state.is_running:
+        if game_state.is_running:
             is_key_pressed = pygame.key.get_pressed()
-            if not game_state.message_displayed:
-                if is_key_pressed[pygame.K_RIGHT]:
-                    game_state.player_ship.rotate(clockwise=True)
-                elif is_key_pressed[pygame.K_LEFT]:
-                    game_state.player_ship.rotate(clockwise=False)
-                if is_key_pressed[pygame.K_UP]:
-                    game_state.player_ship.accelerate(acceleration_factor=0.5)
-                elif is_key_pressed[pygame.K_DOWN]:
-                    game_state.player_ship.deccelerate(deceleration_factor=0.5)
+            if is_key_pressed[pygame.K_RIGHT]:
+                game_state.player_ship.rotate(clockwise=True)
+            elif is_key_pressed[pygame.K_LEFT]:
+                game_state.player_ship.rotate(clockwise=False)
+            if is_key_pressed[pygame.K_UP]:
+                game_state.player_ship.accelerate(acceleration_factor=0.5)
+            elif is_key_pressed[pygame.K_DOWN]:
+                game_state.player_ship.deccelerate(deceleration_factor=0.5)
